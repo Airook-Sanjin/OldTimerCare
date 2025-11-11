15d9d9d2335789f6ba5e37f43ed9fc26bbedb3c8
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-11-11 16:30:12 -0500

    Test

---
+++ resources/views/Users/homebase.blade.php
@@ -0,0 +1,6 @@
+@extends('Welcome');
+@section('title','Home');
+@section('Content');
+@csrf  <!--This is hidden input field with a unique token So no cross-site  attacks  -->
+<!-- Put the html in here for homebase -->
+@endsection
