c3c4eb2f7c67dddf17c4f380e99f49d9e185ce5c
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-11-20 08:19:10 -0500

    little-update

--- resources/views/Users/homebase.blade.php
+++ resources/views/Users/homebase.blade.php
@@ -1,9 +1,19 @@
-@extends('Welcome');
-@section('title','Home');
-@section('Content');
+@extends('Header&Footer')
+@section('title','Home')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/UserCSS/Homebase.css') }}">
+@endsection
+
+@section('content')
 @csrf  <!--This is hidden input field with a unique token So no cross-site  attacks  -->
 <!-- Put the html in here for homebase -->
 
-
+<div class="Appointment">
+    <p>Hello</p> 
+</div>
+<div class="Checklist"> </div>
+<div class="Current">   </div>
+<div class="Medicine"></div>
 
 @endsection
