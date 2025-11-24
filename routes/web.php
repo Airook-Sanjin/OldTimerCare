19c8ee6944c046fbf2fe907603b2514a621a6035
Israel Fernandez <izzyyzzi5@gmail.com>
2025-11-24 08:41:25 -0500

    Created registration and login
    created registration and login

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PatientRegistrationController;
+use App\Http\Controllers\FamilyRegistrationController;
+use App\Http\Controllers\AdminApprovalController;
 
 Route::get('/', function () {
-    return view('Users\homebase');
+    return view('Users.homebase');
 });
+
+// Login + Logout
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Registration
+Route::get('/register/patient', [PatientRegistrationController::class, 'showForm'])->name('register.patient');
+Route::post('/register/patient', [PatientRegistrationController::class, 'register']);
+
+Route::get('/register/family', [FamilyRegistrationController::class, 'showForm'])->name('register.family');
+Route::post('/register/family', [FamilyRegistrationController::class, 'register']);
+
+// Admin approval panel
+Route::get('/admin/approvals', [AdminApprovalController::class, 'index'])->name('admin.approvals');
+
+// Approve actions
+Route::post('/admin/approve/patient/{id}', [AdminApprovalController::class, 'approvePatient']);
+Route::post('/admin/approve/family/{id}', [AdminApprovalController::class, 'approveFamily']);
+
+// Reject actions
+Route::post('/admin/reject/patient/{id}', [AdminApprovalController::class, 'rejectPatient']);
+Route::post('/admin/reject/family/{id}', [AdminApprovalController::class, 'rejectFamily']);
