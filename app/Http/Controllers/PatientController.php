87cf8ca4a607272be81644f6fb20bc1d811815fb
Airook <181858985+Airook-Sanjin@users.noreply.github.com>
2025-12-03 08:39:00 -0500

    Fixed the smallest of errors

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -13,7 +13,7 @@ class PatientController extends Controller
     public function index()
     {   
         $user = auth()->user();
-       return view('Users.Patient.home'); //
+       return view('Users.Patient.home',compact('user')); //
     }
 
     /**
