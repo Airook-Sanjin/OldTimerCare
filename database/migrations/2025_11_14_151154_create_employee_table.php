6cd089bc3ce3e9bab5ababca804b348d9dfc04bd
Darlin <darli2819@gmail.com>
2025-12-09 12:32:49 -0500

    fixed patient register

--- database/migrations/2025_11_14_151154_create_employee_table.php
+++ database/migrations/2025_11_14_151154_create_employee_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
 
             $table->foreign('UserID')->references('UserID')->on('Users');
             $table->foreign('RoleID')->references('RoleID')->on('Role');
+            
         });
     }
 
